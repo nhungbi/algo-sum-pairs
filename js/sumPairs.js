7201e2099dc817bfefc83ee54f96ff13111ccf6e
nhungbi <nhungvan16@gmail.com>
2022-05-25 19:41:49 -0500

    fn!

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,17 +1,19 @@
 exports.sumPairs = function(integerArrays, desiredSum) {
     let output = []
-    for (let i=0; i < integerArrays.length; i++) {
-        for (let int of integerArrays.slice(i)) {
+    for (let i=0; i < integerArrays.length; i++) { 
+        for (let int of integerArrays.slice(i)) { //find all possible pairings
             if (integerArrays[i] + int === desiredSum) {
                 output.push([integerArrays[i], int])
             }
         }}
     
     if (output.length != 0) {
-        return output
-    } 
+        return JSON.stringify(output)} 
+    
+        
     return 'unable to find pair'
 
 };
-console.log(exports.sumPairs([1,2,3,4,5], 9))
-console.log(exports.sumPairs([1,1,1], 6))
+//console.log(exports.sumPairs([1,2,3,4,5], 9))
+//console.log(Array(2))
+//console.log(exports.sumPairs([1,1,3], 2)))
